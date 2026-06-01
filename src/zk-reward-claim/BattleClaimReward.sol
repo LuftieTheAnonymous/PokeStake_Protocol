4533206cca9ea91f6f569a5f6e8e01c7836e5678
LuftieTheAnonymous <luftjunkie@protonmail.com>
2026-06-01 10:02:20 +0100

    Mark subproject commits as dirty and refactor BattleClaimReward contract to improve reward claiming logic and add IncrementableMerkleTree functionality

--- src/zk-reward-claim/BattleClaimReward.sol
+++ src/zk-reward-claim/BattleClaimReward.sol
@@ -1,74 +1,102 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.27;
 
 import {IVerifier} from "./Verifier.sol";
-
 import {SnorlieCoin} from "../PokeCoin.sol";
+import {IncrementableMerkleTree} from "./IMTree.sol";
 
-import {AccessControl} from "../../lib/openzeppelin-contracts/contracts/access/AccessControl.sol";
+contract BattleClaimReward is IncrementableMerkleTree {
+    error InvalidProof();
+    error Invalid_MerkleProof();
+    error NullifierHash_AlreadyUsed(bytes32 _nullifierHash);
+    error LeafAlreadyUsed(bytes32 _leaf);
 
-contract BattleClaimReward is AccessControl {
+    event RewardClaimed(
+        address indexed claimer,
+        bytes32 indexed nullifierHash,
+        bytes32 indexed leaf,
+        uint256 blockNumber
+    );
 
-error InvalidProof();
-error Invalid_MerkleRoot(bytes32 _root);
-error NullifierHash_AlreadyUsed(bytes32 _nullifierHash);
+    IVerifier verifier;
+    SnorlieCoin snorlieCoin;
 
-event RewardClaim(bytes _proof, bytes32 _nullifierHash, uint256 blockNumber);
-event Added_MerkleRoot(bytes32 _merkleRoot, uint256 addedAtBlock);
-
-IVerifier verifier;
-SnorlieCoin snorlieCoin;
-
-bytes32 ADMIN_ROLE = bytes32("ADMIN_ROLE");
-mapping(bytes32 _root => bool isValid) private validMerkleRoots;
-mapping(bytes32 nullifierHash => bool isUsed) private nullifierHashUsed;
-uint256 constant private REWARD_BASE = 2e18;
-uint256 FIELD_SIZE = 21888242871839275222246405745257275088548364400416034343698204186575808495617;
-
-constructor(IVerifier _verifier, SnorlieCoin _snorlieCoin){
-    verifier = _verifier;
-    snorlieCoin = _snorlieCoin;
-    _grantRole(ADMIN_ROLE, msg.sender);
-}
-
-
-function rewardClaim(bytes memory _proof, bytes32 _nullifierHash, bytes32 _hashSecret, bytes32 _merkleRoot) external {
+    mapping(bytes32 nullifierHash => bool isUsed) private nullifierHashUsed;
+    mapping(bytes32 leaf => bool isUsed) private leafUsed;
     
-    if(!validMerkleRoots[_merkleRoot]){
-        revert Invalid_MerkleRoot(_merkleRoot);
+    uint256 private constant REWARD_BASE = 2e18;
+    uint256 public constant FIELD_SIZE =
+        21888242871839275222246405745257275088548364400416034343698204186575808495617;
+
+    constructor(IVerifier _verifier, SnorlieCoin _snorlieCoin) 
+        IncrementableMerkleTree() 
+    {
+        verifier = _verifier;
+        snorlieCoin = _snorlieCoin;
     }
 
-    if(nullifierHashUsed[_nullifierHash]){
-        revert NullifierHash_AlreadyUsed(_nullifierHash);
+    /**
+     * User submits proof to claim reward
+     * 
+     * @param _proof - ZK proof from barretenberg (encodes game state validity)
+     * @param _nullifierHash - hash(nullifier) to prevent double claims
+     * @param _hashSecret - hash of secret value
+     * @param _gameStateHash - leaf data representing game outcome (winner, loser, etc)
+     * @param _merkleProof - merkle path to prove leaf is in tree
+     */
+    function rewardClaim(
+        bytes memory _proof,
+        bytes32 _nullifierHash,
+        bytes32 _hashSecret,
+        bytes32 _gameStateHash,
+        bytes32[] memory _merkleProof
+    ) external {
+        // Prevent double claims via nullifier
+        if (nullifierHashUsed[_nullifierHash]) {
+            revert NullifierHash_AlreadyUsed(_nullifierHash);
+        }
+
+        // Prevent same leaf from being claimed twice
+        if (leafUsed[_gameStateHash]) {
+            revert LeafAlreadyUsed(_gameStateHash);
+        }
+
+        // Add leaf to tree (first time it's seen)
+        _addLeaf(_gameStateHash);
+
+        // Verify merkle proof (leaf is in tree)
+        if (!_verifyMerkleProof(_gameStateHash, leaves.length - 1, _merkleProof)) {
+            revert Invalid_MerkleProof();
+        }
+
+        // Prepare public inputs for ZK proof
+        bytes32[] memory _publicInputs = new bytes32[](4);
+        _publicInputs[0] = _nullifierHash;
+        _publicInputs[1] = _hashSecret;
+        _publicInputs[2] = bytes32(uint256(uint160(msg.sender)) % FIELD_SIZE);
+        _publicInputs[3] = getCurrentRoot();
+
+        // Verify zero-knowledge proof
+        bool proofValidity = verifier.verify(_proof, _publicInputs);
+        if (!proofValidity) {
+            revert InvalidProof();
+        }
+
+        // Mark as claimed
+        nullifierHashUsed[_nullifierHash] = true;
+        leafUsed[_gameStateHash] = true;
+
+        // Mint reward
+        snorlieCoin.mint(msg.sender, REWARD_BASE);
+
+        emit RewardClaimed(msg.sender, _nullifierHash, _gameStateHash, block.number);
     }
 
-    bytes32[] memory _publicInputs = new bytes32[](4);
-    _publicInputs[0] = _nullifierHash;
-    _publicInputs[1] = _hashSecret;
-     _publicInputs[2] = bytes32(uint256(uint160(msg.sender)) % FIELD_SIZE);
-    _publicInputs[3] = _merkleRoot; 
-
-    bool proofValidity = verifier.verify(_proof, _publicInputs);
-
-    if(!proofValidity){
-        revert InvalidProof();
+    function isNullifierUsed(bytes32 _nullifierHash) external view returns (bool) {
+        return nullifierHashUsed[_nullifierHash];
     }
 
-    // Disables from double usage
-    validMerkleRoots[_merkleRoot] = false;
-
-    nullifierHashUsed[_nullifierHash]=true;
-
-    snorlieCoin.mint(msg.sender, REWARD_BASE);
-
-    emit RewardClaim(_proof, _nullifierHash, block.number);
-}
-
-function addValidMerkleRoot(bytes32 _merkleRoot) external onlyRole(ADMIN_ROLE) {
-    validMerkleRoots[_merkleRoot] = true;
-
-    emit Added_MerkleRoot(_merkleRoot, block.number);
-}
-
+    function isLeafUsed(bytes32 _leaf) external view returns (bool) {
+        return leafUsed[_leaf];
+    }
 }
